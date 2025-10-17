3e890262c78187a8ab6ffe3dd5362b17cd34d533
Salty / Moris <vertixo.vider05@gmail.com>
2025-10-18 02:41:19 +0300

    Fix camera bug
    Fixed camera stuck in one angle

--- driving-sim-ee/CamArm.gd
+++ driving-sim-ee/CamArm.gd
@@ -12,4 +12,4 @@ func _input(event):
 		rotation_degrees.x = clamp(rotation_degrees.x, -90.0, -10.0)
 
 		rotation_degrees.y -= event.relative.x * MouseSensitivity
-		rotation_degrees.y = wrapf(rotation_degrees.y, -90.0, -10.0)
+		rotation_degrees.y = wrapf(rotation_degrees.y, 0.0, 360.0)
