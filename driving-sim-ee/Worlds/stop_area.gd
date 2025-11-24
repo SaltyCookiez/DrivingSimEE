a8026d29d63f4499163806438ac7f54c3d8aa2ed
Salty / Moris <vertixo.vider05@gmail.com>
2025-11-24 15:28:07 +0200

    Added function if the body entered stop sign area

--- driving-sim-ee/Worlds/stop_area.gd
+++ driving-sim-ee/Worlds/stop_area.gd
@@ -9,3 +9,12 @@ extends Area3D
 var car: Node3D = null
 var time_below_threshold: float = 0.0
 var car_has_stopped: bool = false
+
+func _on_body_entered(body: Node3D) -> void:
+	# Check if the body is car
+	if body.is_in_group("car"):
+		car = body
+		time_below_threshold = 0.0
+		car_has_stopped = false
+		# Print debug for testing rn
+		print("Car is at the stop sign")
