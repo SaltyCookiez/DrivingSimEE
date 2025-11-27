f9f3bd15aa6f448f8d782158b7435d3b19234881
Salty / Moris <vertixo.vider05@gmail.com>
2025-11-27 17:32:42 +0200

    Camera fixing#1
    Camera stays in place, player mouse input only moves camera up and down. Next step is to fix the camera rotation, camera rotation speed is faster than cars turning speed

--- driving-sim-ee/CamArm.gd
+++ driving-sim-ee/CamArm.gd
@@ -1,28 +1,33 @@
 extends SpringArm3D
 
+@export var car: VehicleBody3D
+
 var MouseSensitivity = 0.1
-var auto_turn_speed := 5.0
-var car = null
+var pitch_deg := -10.0
+@export var yaw_offset_degrees := -90.0
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	set_as_top_level(true)
-	car = get_parent()
+	# set_as_top_level(true)
+	var cam := get_node_or_null("Camera3D")
+	if cam:
+		cam.make_current()
+	if car == null and get_parent() is VehicleBody3D:
+		car = get_parent() as VehicleBody3D
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		rotation_degrees.x -= event.relative.y * MouseSensitivity
-		rotation_degrees.x = clamp(rotation_degrees.x, -90.0, -10.0)
+		pitch_deg -= event.relative.y * MouseSensitivity
+		pitch_deg = clamp(pitch_deg, -15.0, 10.0)
+		rotation_degrees.x = pitch_deg
 
-		rotation_degrees.y -= event.relative.x * MouseSensitivity
-		rotation_degrees.y = wrapf(rotation_degrees.y, 0.0, 360.0)
-
-func _process(delta):
+func _process(_delta):
 	if car == null:
 		return
-	
-	var vel = car.linear_velocity
-	
-	if vel.length() > 0.1:
-		var target_yaw = atan2(-vel.x, -vel.z) * 180.0 / PI
-		rotation_degrees.y = lerp_angle(rotation_degrees.y, target_yaw, auto_turn_speed * delta)
+
+	global_position = car.global_transform.origin
+
+	var car_euler: Vector3 = car.global_transform.basis.get_euler()
+	var yaw := car_euler.y + deg_to_rad(yaw_offset_degrees)
+
+	rotation = Vector3(deg_to_rad(pitch_deg), yaw, 0.0)
