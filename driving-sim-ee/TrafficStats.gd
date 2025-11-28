9b04fdaa020d27ca0808ebeb0a3065abe478f814
Salty / Moris <vertixo.vider05@gmail.com>
2025-11-28 21:50:32 +0200

    Update TrafficStats.gd script

--- driving-sim-ee/TrafficStats.gd
+++ driving-sim-ee/TrafficStats.gd
@@ -1,1 +1,16 @@
 extends Node
+
+var give_way_correct := 0
+var give_way_wrong := 0
+
+var stop_correct := 0
+var stop_wrong := 0
+
+var odd_parking_correct := 0
+var odd_parking_wrong := 0
+
+# Mock version for current day. After creating a test level with odd or not odd date parking sign, update this mock version
+var current_day := 5 # 5th day of the month (odd)
+
+func is_odd_day() -> bool:
+	return current_day % 2 != 0
