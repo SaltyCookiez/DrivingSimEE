a376b14baf1c3eb5d4f92452d4ca264b2fea0584
Salty / Moris <vertixo.vider05@gmail.com>
2025-12-05 01:41:22 +0200

    POV/First Person Camera works after fixing Typo (01.12)

--- driving-sim-ee/Car.gd
+++ driving-sim-ee/Car.gd
@@ -174,7 +174,7 @@ func _physics_process(delta):
 		#_print_debug(speed)
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("camera_switch"):
+	if event.is_action_pressed("Camera_Switch"):
 		set_pov(not is_pov)
 
 # Engine and Transmission
