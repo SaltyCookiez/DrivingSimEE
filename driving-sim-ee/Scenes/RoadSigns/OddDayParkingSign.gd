83d4002c825dbbed3b789a25b4980ea9bb1fee27
Salty / Moris <vertixo.vider05@gmail.com>
2025-12-05 06:07:58 +0200

    Working OddDayParkingSign (02.12)

--- driving-sim-ee/Scenes/RoadSigns/OddDayParkingSign.gd
+++ driving-sim-ee/Scenes/RoadSigns/OddDayParkingSign.gd
@@ -1,7 +1,5 @@
 extends Node3D
 
-# Attach to sign later
-
 @export var parking_detect_time: float = 5.0
 @export var parking_speed: float = 2.0
 
@@ -10,6 +8,12 @@ extends Node3D
 var _car: Node3D = null
 var _car_inside := false
 var _stopped_time: float = 0.0
+var _already_reported: bool = false
+var _car_has_parked: bool = false
+
+func _ready() -> void:
+	detection_area.body_entered.connect(body_entered)
+	detection_area.body_exited.connect(body_exited)
 
 func body_entered(body: Node3D) -> void:
 	if not body.is_in_group("car"):
@@ -18,45 +22,41 @@ func body_entered(body: Node3D) -> void:
 	_car = body
 	_car_inside = true
 	_stopped_time = 0.0
+	_already_reported = false
 
 func body_exited(body: Node3D) -> void:
-	if not body.is_in_group("car"):
+	if body != _car:
 		return
 
-	_car_inside = false
-
-	var was_parking := _stopped_time >= parking_detect_time
-	var is_odd_day_now := TrafficStats.is_odd_day()
-
-	if was_parking:
-		if is_odd_day_now:
-			TrafficStats.odd_parking_correct += 1
-			print("Odd-day NO-PARKING: violation, parked for", _stopped_time, "seconds")
-		else:
-			TrafficStats.odd_parking_wrong += 1
-			print("Odd-day NO-PARKING: parked but day is allowed")
-	else:
-		# Car passed through without parking
+	if not _car_has_parked:
 		TrafficStats.odd_parking_ok += 1
-		print("Odd-day NO-PARKING: did not park")
+		print("NO-PARKING ODD DAY: car passed without parking")
 
+	_car_inside = false
 	_car = null
-
-func _ready() -> void:
-	detection_area.body_entered.connect(body_entered)
-	detection_area.body_exited.connect(body_exited)
+	_stopped_time = 0.0
+	_already_reported = false
 
 func _physics_process(delta: float) -> void:
 	if not _car_inside or _car == null:
 		return
 
-	if not _car.has_variable("speed_kmh"):
+	var current_speed: float = abs(_car.speed_kmh)
+
+	if current_speed > parking_speed:
+		_stopped_time = 0.0
 		return
 
-	var current_speed = _car.speed_kmh
+	_stopped_time += delta
 
-	if abs(current_speed) <= parking_speed:
-		_stopped_time += delta
-	else:
-		# Reset if car moves again
-		_stopped_time = 0.0
+	if _stopped_time >= parking_detect_time and not _already_reported:
+		_already_reported = true
+
+		var is_odd_day_now: bool = TrafficStats.is_odd_day()
+
+		if is_odd_day_now:
+			TrafficStats.odd_parking_correct += 1
+			print("NO-PARKING ODD DAY: violation (parked for ", _stopped_time, " sec)")
+		else:
+			TrafficStats.odd_parking_wrong += 1
+			print("NO-PARKING: allowed parking today")
