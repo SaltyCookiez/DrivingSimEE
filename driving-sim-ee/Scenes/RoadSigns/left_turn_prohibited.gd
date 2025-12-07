741a29730f9873f03f51d3a74b55b52fafe9db32
Salty / Moris <vertixo.vider05@gmail.com>
2025-12-07 15:01:45 +0200

    Configured all scripts for signs (except pedestrian for now)

---
+++ driving-sim-ee/Scenes/RoadSigns/left_turn_prohibited.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+@onready var detection_area: Area3D = $DetectionArea
+var _triggered := false  # double counting fix
+
+func _ready() -> void:
+	detection_area.body_entered.connect(_on_body_entered)
+	detection_area.body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body: Node3D) -> void:
+	if not body.is_in_group("car"):
+		return
+	if _triggered:
+		return
+	_triggered = true
+
+	TrafficStats.left_turn_wrong += 1
+
+	print("Sign fault detected")
+
+func _on_body_exited(body: Node3D) -> void:
+	if not body.is_in_group("car"):
+		return
+	_triggered = false
